98473529ad7950cefce955404c6931a9caa4197e
ritik sharma <sharmaritik896@gmail.com>
2022-07-28 23:38:03 +0530

    day 25 continues

--- day-25/main.py
+++ day-25/main.py
@@ -1,2 +1,13 @@
-with open('. /weather-data.csv') as file:
-    print(file.readlines())
+# with open("weather-data.csv") as file:
+# data = file.readlines()
+# print(data)
+#
+import csv
+
+with open("weather-data.csv")as data_file:
+    data = csv.reader(data_file)
+    temp = []
+    for row in data:
+        if row[1] != 'temp':
+            temp.append(int(row[1]))
+    print(temp)
