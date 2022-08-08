b07aab929106e334006f1cb2d1064e8a86084c55
ritik sharma <sharmaritik896@gmail.com>
2022-08-08 23:48:30 +0530

    error and exception

--- day-30/main.py
+++ day-30/main.py
@@ -1,3 +1,28 @@
-a = 50
-print(a)
-"""this is judt to stsy in th"""
+# # with open('a_file.text') as file:
+# #     file.read()
+
+
+# from importlib.resources import open_binary
+
+
+# try:
+#     file = open('a_foile.text')
+
+# except:
+#     print("there was a problem")
+# # else
+
+fruits = ['apple', 'pear', 'orange']
+
+
+def make_pie(index):
+    try:
+        fruit = fruits[index]
+        print(fruit + 'pie')
+    except IndexError:
+        print('fruit pie')
+    else:
+        print(fruit+'pie')
+
+
+make_pie(4)
